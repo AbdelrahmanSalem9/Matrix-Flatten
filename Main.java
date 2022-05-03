863e933552dc1d7756a637308681798f76a80b0d
AbdelrahmanSalem9 <abdelrahmansalem16@gmail.com>
2022-05-03 21:33:05 +0200

    modifed UI

--- Main.java
+++ Main.java
@@ -51,6 +51,7 @@ public class Main {
                     System.out.println(array[mFlatten.getMappedIndex(i, j, k)]);
                 } else {
                     System.out.println("Invalid Test Input Please Try Again...");
+                    System.out.println("------------------------------------------------");
                     continue;
                 }
                 System.out.println("------------------------------------------------");
